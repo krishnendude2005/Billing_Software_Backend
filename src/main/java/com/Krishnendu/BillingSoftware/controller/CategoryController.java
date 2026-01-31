acccd62c7ee8098fe917e5c6738bb75fb1ad8a1f
Krishnendu De <krishnendu1205.be22@chitkarauniversity.edu.in>
2026-01-31 18:48:19 +0530

    Created method for new category add

--- src/main/java/com/Krishnendu/BillingSoftware/controller/CategoryController.java
+++ src/main/java/com/Krishnendu/BillingSoftware/controller/CategoryController.java
@@ -2,6 +2,10 @@ package com.Krishnendu.BillingSoftware.controller;
 
 import com.Krishnendu.BillingSoftware.io.CategoryRequest;
 import com.Krishnendu.BillingSoftware.io.CategoryResponse;
+import com.Krishnendu.BillingSoftware.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,11 +13,24 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/categories")
+@RequiredArgsConstructor
 public class CategoryController {
 
-    @PostMapping("/add")
-    public CategoryResponse addCategory(@RequestBody CategoryRequest categoryRequest) {
+    private final CategoryService categoryService;
 
-        return null;
+    @PostMapping
+    public ResponseEntity<CategoryResponse> addCategory(@RequestBody CategoryRequest categoryRequest) {
+
+        CategoryResponse response = categoryService.add(categoryRequest);
+
+        if(response.getCategoryId()==null){
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(response);
+        }
+
+        return  ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(response);
     }
 }
