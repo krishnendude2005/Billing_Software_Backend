16b100045d633e0a00975fea3c0e1ab8e9e9e1b4
Krishnendu De <krishnendu1205.be22@chitkarauniversity.edu.in>
2026-02-07 23:15:41 +0530

    Implemented the delte user endpoint functionality

--- src/main/java/com/Krishnendu/BillingSoftware/controller/UserController.java
+++ src/main/java/com/Krishnendu/BillingSoftware/controller/UserController.java
@@ -8,10 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -42,5 +39,11 @@ public class UserController {
        List<UserResponse> users = userService.readAllUsers();
        return
                ResponseEntity.status(HttpStatus.OK).body(users);
+    }
+
+    @DeleteMapping("/user/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable String id) {
+        userService.deleteUser(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
     }
 }
